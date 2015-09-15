289bc7d9d88671f19f8febd9383a8845b9e77a2f
Matthew Berginski <matthew.berginski@gmail.com>
2015-09-15 14:13:50 -0400

    adding banding quantification to ratio images

--- stain_quant.m
+++ stain_quant.m
@@ -13,6 +13,8 @@ i_p.addParameter('second_search_str','*FWTR.TIF',@(x)ischar(x));
 i_p.addParameter('background_threshold',300,@(x)isnumeric(x));
 i_p.addParameter('background_min_size',250000,@(x)isnumeric(x));
 
+i_p.addParameter('band_size',100,@(x)isnumeric(x));
+
 i_p.parse(exp_dir,varargin{:});
 
 edge_files = dir(fullfile(exp_dir,i_p.Results.edge_search_str));
@@ -30,9 +32,15 @@ addpath(genpath(misc_image_processing_dir));
 
 intensity_measurements = [];
 
-for i = 1:length(secondary_files)
-    this_secondary_file = fullfile(exp_dir,secondary_files(i).name);
-    this_edge_file = fullfile(exp_dir,edge_files(i).name);
+%this should be big enough to hold all the results from banding the
+%intensity values
+ratio_bands = zeros(length(secondary_files),10000);
+
+max_band_count = 0;
+
+for file_num = 1:length(secondary_files)
+    this_secondary_file = fullfile(exp_dir,secondary_files(file_num).name);
+    this_edge_file = fullfile(exp_dir,edge_files(file_num).name);
     
     secondary = double(imread(this_secondary_file));
     edge_image = double(imread(this_edge_file));
@@ -90,6 +98,7 @@ for i = 1:length(secondary_files)
     imwrite(secondary_image_highlight,fullfile(path,[sec_name,'_secondary_edge.png']));
     
     imwrite(ratio_image,fullfile(path,[sec_name,'_ratio.png']));
+    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Data Collection
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -97,6 +106,26 @@ for i = 1:length(secondary_files)
         mean(edge_image(edge_binary)),mean(secondary(edge_binary)),...
         mean(ratio_image(edge_binary)),mean(edge_image(vesicle_binary)),...
         mean(secondary(vesicle_binary)),mean(ratio_image(vesicle_binary))]; %#ok<AGROW>
+    
+    %Ratio Image Banding
+    % Check to make sure there is a background region found, otherwise,
+    % infinite loop
+    if (any(background_region(:)))
+        background_dist = bwdist(background_region);
+        
+        band_limits = [0, i_p.Results.band_size];
+        band_counter = 1;
+        while (band_limits(1) <= max(background_dist(:)))
+            this_band = background_dist > band_limits(1) & background_dist <= band_limits(2);
+            ratio_bands(file_num,band_counter) = mean(ratio_image(this_band));
+            
+            band_counter = band_counter + 1;
+            band_limits = band_limits + i_p.Results.band_size;
+        end
+        
+        if (band_counter > max_band_count), max_band_count = band_counter; end
+    end
+    
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -106,3 +135,14 @@ headers = {'Edge in Edge','Edge in 2nd','Edge in Ratio','Vesicle in Edge',...
     'Vesicle in 2nd','Vesicle in Ratio'};
 csvwrite_with_headers(fullfile(exp_dir,'quantification.csv'),intensity_measurements,...
     headers);
+
+ratio_bands = ratio_bands(:,1:max_band_count);
+ratio_headers = cell(0);
+band_limits = [0, i_p.Results.band_size];
+for i = 1:size(ratio_bands,2)
+    ratio_headers{i} = sprintf('%d - %d',band_limits(1),band_limits(2));
+    band_limits = band_limits + i_p.Results.band_size;
+end
+
+csvwrite_with_headers(fullfile(exp_dir,'ratio_band_means.csv'),ratio_bands,...
+    ratio_headers);
